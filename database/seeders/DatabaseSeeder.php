9b1679e17db8a8cef35b9bd31217241d30672c61
Bayik4 <ayikarbail@gmail.com>
2024-01-13 22:13:34 +0700

    menghapus file seeder member dan mengubah file databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,48 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as faker;
+use App\Models\Member;
+use App\Models\User;
+use App\Models\Buku;
+use App\Models\Penjaga;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Member::create([
+            'nama' => 'ayikarbailidia',
+            'alamat' => 'kab. Nganjuk',
+            'umur' => '19',
+            'jenis_kelamin' => 'laki-laki',
+            'email' => 'ayikarbail@gmail.com',
+            'no_telp' => '08571234567'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'ayikarbail',
+            'email' => 'ayikarbail@gmail.com',
+            'password' => 'arbailayik12'
+        ]);
+
+        $faker = Faker::create('id_ID');
+        for($i = 0; $i < 10; $i++) {
+            Buku::create([
+                'nomor_buku' => $faker->numberBetween(100, 500),
+                'judul_buku' => $faker->word,
+                'jenis_buku' => $faker->word,
+                'nomor_rak' => $faker->numberBetween(1, 5)
+            ]);
+        }
+
+        Penjaga::create([
+            'nama' => 'kutubuku',
+            'alamat' => 'Kab. malang',
+            'umur' => '25',
+            'jenis_kelamin' => 'laki-laki',
+            'email' => 'kutubuku@gmail.com',
+            'no_telp' => '085813310'
+        ]);
     }
 }
